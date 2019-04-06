c8e110d017eadad7936d35d47b79ce3228059dd1
Dany Cortes <daniela.munozc@gmail.com>
2019-04-05 19:40:55 -0600

    Feature | Adding new components
    - Adding home, login and portfolio administrator components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PortfolioAdminComponent } from './portfolio-admin/portfolio-admin.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        PortfolioAdminComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
---
+++ src/app/portfolio-admin/portfolio-admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-portfolio-admin',
+    templateUrl: './portfolio-admin.component.html',
+    styleUrls: ['./portfolio-admin.component.scss']
+})
+
+
+export class PortfolioAdminComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
