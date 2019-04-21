61c48521d99257c999971a4f17f7446ed4c73b41
Dany Cortes <daniela.munozc@gmail.com>
2019-04-20 20:50:09 -0500

    Feature | About component added into home module
    - About component added
    - Styles added
    - Data added by component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,40 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 export class HomeComponent implements OnInit {
     jobTitle: string;
+    aboutTitle: string;
+    aboutIntro: string;
+    skillsTitle: string;
+    technicalSkills: Array<Object>;
+
     @ViewChild('about') about: ElementRef;
+    @ViewChild('portfolio') portfolio: ElementRef;
 
 
     ngOnInit() {
         this.jobTitle = 'Front-end Developer';
+        this.aboutTitle = 'Quién soy';
+        this.aboutIntro = `¡Hola! soy Dany, diseñadora y desarrolladora de solucionas web.
+        Los últimos 4 años me he desenvuelto principalmente en la programación de aplicaciones web
+        del lado del cliente. Actualmente formo parte de un equipo de desarrolladores enfocados en
+        generar proyectos de tecnología educativa, ahí he tenido la oportunidad de ser Líder de Proyectos.
+        Me encantan los retos que involucra mi profesión pues resultan siempre en un aprendizaje
+        autónomo y evolutivo.
+        Tengo background de diseñadora, por lo que es importante para mí aportar conocimientos
+        visuales a los proyectos en los que trabajo.`;
+        this.skillsTitle = 'Habilidades Técnicas';
+        this.technicalSkills = [
+            { name: 'HTML5' },
+            { name: 'CSS3' },
+            { name: 'SASS' },
+            { name: 'JavaScript' },
+            { name: 'jQuery' },
+            { name: 'Wordpress' },
+            { name: 'AngularJs' },
+            { name: 'Angular' },
+            { name: 'Diseño Responsivo' },
+            { name: 'Git' },
+            { name: 'UI/UX' }
+        ];
     }
 
 
@@ -26,6 +55,9 @@ export class HomeComponent implements OnInit {
         switch ( section ) {
             case 'about':
                 this.about.nativeElement.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+                break;
+            case 'portfolio':
+                this.portfolio.nativeElement.scrollIntoView( { behavior: 'smooth', block: 'start' } );
                 break;
         }
     }
