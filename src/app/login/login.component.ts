3b40e52610b1c8e6915aad0ea5efe6a08c4db07e
Daniela Cortes <danycortes@Danielas-MacBook-Air.local>
2019-07-25 19:08:51 -0500

    Feature | Adding reactive forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -9,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class LoginComponent implements OnInit {
-    constructor() { }
+    loginForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+
+
     ngOnInit() {
+        this.buildForm();
     }
+
+    buildForm(){
+        this.loginForm = this.formBuilder.group({
+            email: ['', [
+                Validators.required,
+                Validators.email
+            ]],
+            password: ['', [
+                Validators.required
+            ]]
+        })
+    } 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRouting } from './login.routing';
 import { LoginComponent } from './login.component';
@@ -8,7 +9,9 @@ import { LoginComponent } from './login.component';
 @NgModule({
     imports: [
         CommonModule,
-        LoginRouting
+        LoginRouting,
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         LoginComponent
