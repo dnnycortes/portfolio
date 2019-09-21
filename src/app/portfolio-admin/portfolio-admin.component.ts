4d04cd749a7c4c6ddff65831cdc2882519ce0204
dnnycortes <daniela.munozc@gmail.com>
2019-09-20 22:07:49 -0500

    Feature | Getting user's data after logged in

--- src/app/portfolio-admin/portfolio-admin.component.ts
+++ src/app/portfolio-admin/portfolio-admin.component.ts
@@ -11,12 +11,25 @@ import { Router } from '@angular/router';
 
 
 export class PortfolioAdminComponent implements OnInit {
+    currentUser: Object;
+
     constructor(
         private router: Router,
         private authService: AuthService
     ) { }
 
     ngOnInit() {
+        this.getCurrentUser();
+    }
+
+    /**
+     * Function to get current user data, right after he was logged in
+     */
+    getCurrentUser() {
+        this.authService.user$.subscribe( (user) => {
+            this.currentUser = user;
+            console.log( this.currentUser );
+        });
     }
 
     /**
