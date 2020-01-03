ad6ceee0d5819648bfda46df5a13f4822948cfa4
dnnycortes <daniela.munozc@gmail.com>
2020-01-03 17:34:30 -0600

    Update | Changing social media icons on contact component

--- src/app/home/components/contact/contact.component.ts
+++ src/app/home/components/contact/contact.component.ts
@@ -16,11 +16,9 @@ export class ContactComponent implements OnInit {
     ngOnInit() {
         this.networking = [
             { id: 0, name: 'linkedin', label: 'Linkedin', active: true, link: 'https://www.linkedin.com/in/dnnycortes/'},
-            { id: 5, name: 'behance', label: 'Behance', active: true, link: 'https://www.behance.net/dnnycortes' },
-            { id: 4, name: 'github', label: 'Github', active: true, link: 'https://github.com/dnnycortes' },
-            { id: 3, name: 'medium', label: 'Medium', active: true, link: 'https://medium.com/@dnnyCortes'},
-            { id: 1, name: 'twitter', label: 'Twitter', active: true, link: 'https://twitter.com/dnnyCortes'},
-            { id: 2, name: 'instagram', label: 'Instagram', active: true, link: 'https://www.instagram.com/dnnycortes/'},
+            { id: 1, name: 'dribbble', label: 'Dribbble', active: true, link: 'https://dribbble.com/danycortes' },
+            { id: 2, name: 'github', label: 'Github', active: true, link: 'https://github.com/dnnycortes' },
+            { id: 3, name: 'twitter', label: 'Twitter', active: true, link: 'https://twitter.com/dnnyCortes'}
         ];
     }
 }
