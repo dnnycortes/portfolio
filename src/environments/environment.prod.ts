f9d2f13d87ac6f35e893d2ef8f8a8f5fd0fe2685
dnnycortes <daniela.munozc@gmail.com>
2020-01-06 19:30:51 -0600

    Update | Updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD4-WOOf18uFVjqlVPYD2GVdFSOJMItFOI',
+    authDomain: 'my-portfolio-dbcf5.firebaseapp.com',
+    databaseURL: 'https://my-portfolio-dbcf5.firebaseio.com',
+    projectId: 'my-portfolio-dbcf5',
+    storageBucket: 'my-portfolio-dbcf5.appspot.com'
+  }
 };
